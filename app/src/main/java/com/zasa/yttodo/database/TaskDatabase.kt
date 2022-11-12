5c93ae01a0f9187bfe0fef9c0407f4b06678f2cc
zansangeeth <zansangeeth17@gmail.com>
2022-11-12 21:39:57 +0530

    implemented TaskDatabase.kt class for getting the instances from Room database

---
+++ app/src/main/java/com/zasa/yttodo/database/TaskDatabase.kt
@@ -0,0 +1,39 @@
+package com.zasa.yttodo.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Created by Sangeeth Amirthanathan
+ * on 11/12/2022.
+ */
+
+@Database(entities = arrayOf(TaskEntry::class), version = 1, exportSchema = false)
+abstract class TaskDatabase: RoomDatabase() {
+
+    abstract fun taskDao() : TaskDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        TaskDatabase::class.java,
+                        "task_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
