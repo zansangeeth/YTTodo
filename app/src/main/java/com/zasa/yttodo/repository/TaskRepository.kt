59c1d5baa52ee50d64bcb8e7bb9e5aa3d120fc34
zansangeeth <zansangeeth17@gmail.com>
2022-11-13 10:45:45 +0530

    implemented TaskViewModel.kt for instance communication between model and the view

---
+++ app/src/main/java/com/zasa/yttodo/repository/TaskRepository.kt
@@ -0,0 +1,20 @@
+package com.zasa.yttodo.repository
+
+import androidx.lifecycle.LiveData
+import com.zasa.yttodo.database.TaskDao
+import com.zasa.yttodo.database.TaskEntry
+
+/**
+ * Created by Sangeeth Amirthanathan
+ * on 11/13/2022.
+ */
+class TaskRepository(val taskDao: TaskDao) {
+
+    suspend fun insert(taskEntry: TaskEntry) = taskDao.insert(taskEntry)
+    suspend fun updateData(taskEntry: TaskEntry) = taskDao.update(taskEntry)
+    suspend fun deleteItem(taskEntry: TaskEntry) = taskDao.delete(taskEntry)
+    suspend fun deleteAll(){
+        taskDao.deleteAll()
+    }
+    fun getAllTasks() : LiveData<List<TaskEntry>> = taskDao.getAllTasks()
+}
---
+++ app/src/main/java/com/zasa/yttodo/viewmodel/TaskViewModel.kt
@@ -0,0 +1,55 @@
+package com.zasa.yttodo.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.zasa.yttodo.database.TaskDatabase
+import com.zasa.yttodo.database.TaskEntry
+import com.zasa.yttodo.repository.TaskRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * Created by Sangeeth Amirthanathan
+ * on 11/13/2022.
+ */
+class TaskViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val taskDao = TaskDatabase.getDatabase(application).taskDao()
+
+    private val repository : TaskRepository
+
+    val getAllTasks : LiveData<List<TaskEntry>>
+
+    init {
+        repository = TaskRepository(taskDao)
+        getAllTasks = repository.getAllTasks()
+    }
+
+    fun insert(taskEntry: TaskEntry){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insert(taskEntry)
+        }
+    }
+
+    fun delete(taskEntry: TaskEntry){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.deleteItem(taskEntry)
+        }
+    }
+
+    fun update(taskEntry: TaskEntry){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.updateData(taskEntry)
+        }
+    }
+
+    fun deleteAll(){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.deleteAll()
+        }
+    }
+
+
+}
